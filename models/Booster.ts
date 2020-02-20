53ba5f4926cb30dfbb077c879be228547446da95
skuttenkuler <samkiddmusic@gmail.com>
2020-02-20 15:05:15 -0800

    booster schema

--- models/Booster.ts
+++ models/Booster.ts
@@ -1,17 +1,39 @@
 import * as mongoose from 'mongoose';
 
+export interface InterfaceBoosters extends mongoose.Document {
+    height:number,
+    diameter:number,
+    totalWidth:number, 
+    mass:number, 
+    cores:number, 
+    boosters:number, 
+    stages:number, 
+    engines:number, 
+    burnTime:number, 
+    thrustSea:number, 
+    thrustVac:number, 
+    payloadLEO:number, 
+    payloadGTO:number, 
+    payloadMars:number, 
+    payloadPluto:number
+}
 const BoosterSchema = new mongoose.Schema({
-    number: {type:Number, required: true},
-    date: {type:Date, required:true},
-    time: {type:String, required:true},
-    booster: {type:String, required:true},
-    launchSite: {type:String, required:true},
-    payload: {type:String, required:true},
-    payloadMass: {type:Number, required:true},
-    orbit: {type:String, required:true},
-    missionOutcome: {type:String, required:true},
-    landingOutcome: {type:String, required:true},
+    height: {type:Number, required:true},
+    diameter: {type:Number, required:true},
+    totalWidth: {type:Number, required:true},
+    mass: {type:String, required:true},
+    cores: {type:Number, required:true},
+    boosters: {type:Number, required:true},
+    stages: {type:Number, required:true},
+    engines: {type:Number, required:true},
+    burnTime: {type:Number, required:true},
+    thrustSea: {type:Number, required:true},
+    thrustVac: {type:Number, required:true},
+    payloadLEO: {type:Number, required:true},
+    payloadGTO: {type:Number, required:true},
+    payloadMars: {type:Number, required:true},
+    payloadPluto: {type:Number, required:true}
 });
 
-const Booster = mongoose.model('Launch', BoosterSchema);
+const Booster = mongoose.model<InterfaceBoosters>('Booster', BoosterSchema);
 export default Booster;
