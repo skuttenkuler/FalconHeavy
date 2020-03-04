d8a8c2fc3247b963446fd803d93442b052c42dea
skuttenkuler <samkiddmusic@gmail.com>
2020-03-04 11:34:08 -0800

    express change

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import * as express from "express";
+import express from "express";
 import * as bodyParser from 'body-parser';
 import * as mongoose from 'mongoose';
 import { router } from "./routes/api-route";
