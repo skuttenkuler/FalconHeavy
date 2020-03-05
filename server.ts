3873023586d6dcf79e288cc9fdc44be5d1f44289
skuttenkuler <samkiddmusic@gmail.com>
2020-03-05 14:53:44 -0800

    server.ts express require

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express = require("express");
 import * as bodyParser from 'body-parser';
 import * as mongoose from 'mongoose';
 import { router } from "./routes/api-route";
