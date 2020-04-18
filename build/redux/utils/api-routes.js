961d04ca51539bd7264792d77fcb0cfe5734a63b
skuttenkuler <sam.kuttenk@gmail.com>
2020-04-17 19:57:52 -0700

    @types/node versioning

--- build/redux/utils/api-routes.js
+++ build/redux/utils/api-routes.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = require("axios");
 exports.default = {
     getRocket(rocket) {
-        return axios_1.default.get(`http://localhost:3001/api/rockets/${rocket}`).then(res => res.data);
+        return axios_1.default.get(`https://api.spacexdata.com/v3/rockets/${rocket}`).then(res => res.data);
     }
 };
 //# sourceMappingURL=api-routes.js.map
